8d2116fabafd8a7f969a638ca220c2a63cada91e
Colon <AC068804@Cerner.net>
2020-01-04 10:29:50 -0600

    Moved Javascript out of the index file

--- script.js
+++ script.js
@@ -11,3 +11,32 @@
 </ol>
 <img src="${}">
 */
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+       //pull input from the form
+       let pilotName = document.querySelector("input[name=pilotName]");
+       let copilotName = document.querySelector("input[name=copilotName]");
+       let fuelLevel = document.querySelector("input[name=fuelLevel]");
+       let CargoMass = document.querySelector("input[name=cargoMass]");
+
+       //Checks for empty fields
+       if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === ""|| CargoMass.value === "") {
+           alert("All fields are required!");
+           // stop the form submission
+           event.preventDefault();
+       }
+       //check for Numbers only
+       var regex=/^[0-9]+$/; //regular expression for numbers only
+       if (!fuelLevel.match(regex) || !CargoMass.match(regex))
+       {
+           alert("Fuel Level and Cargo Mass must input numbers");
+           return false;
+       }
+       //Check for text only 
+       if(typeof pilotName !== "string" || typeof(copilotName) !== "string"){
+           alert("Text only ");
+           return false;
+       }
+   });
+});
